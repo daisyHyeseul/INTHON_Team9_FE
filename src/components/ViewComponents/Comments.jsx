cd0f7039d8c02950de8989cb986e68c13e224709
daisyHyeseul <csj0934@naver.com>
2024-11-10 03:44:09 +0900

    fix: style 수정

--- src/components/ViewComponents/Comments.jsx
+++ src/components/ViewComponents/Comments.jsx
@@ -1,16 +1,25 @@
 import PropTypes from 'prop-types';
-import CommentsCont from "./CommentsCont"
+import CommentsCont from './CommentsCont';
 
-export default function Comments ({setComment, mode}){
-
-    return (
-    <div className='flex flex-col justify-start items-start text-white w-5/6 rounded-2xl gap-4' >
-      <h1 className="text-white text-xl">나의 답장</h1>
-      <div className="cont flex flex-col justify-start items-start text-white w-full p-6 rounded-2xl h-auto" style={{backgroundColor:'#5B4C7D'}}>
-        <CommentsCont setComment={setComment} mode={mode} comment={'잘했어'}></CommentsCont>
+// eslint-disable-next-line react/prop-types
+export default function Comments({ comment, setComment, maxLength, mode }) {
+  return (
+    <div className="flex flex-col justify-start items-start text-white w-5/6 rounded-2xl ">
+      <h1 className="text-[#e8dfff] text-xs font-normal mb-[8px]">나의 답장</h1>
+      <div className="cont flex flex-col justify-start items-start w-full bg-[#7763a5]/50 rounded-[18px] p-4 h-[158px] text-white text-xs font-normal">
+        <CommentsCont
+          setComment={setComment}
+          mode={mode}
+          comment={'잘했어'}
+          maxLength={maxLength}
+        ></CommentsCont>
       </div>
+
+      {/* <div className="absolute bottom-0 text-white/60 text-xs font-normal">
+        {comment.length}/{maxLength}
+      </div> */}
     </div>
-    )
+  );
 }
 
 Comments.propTypes = {
