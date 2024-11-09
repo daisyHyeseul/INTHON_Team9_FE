7d370aa44f1bbfb477e6cde3e3300647618914b4
daisyHyeseul <csj0934@naver.com>
2024-11-10 04:00:04 +0900

    fix: 진짜 send 페이지 해결

--- src/pages/Send.jsx
+++ src/pages/Send.jsx
@@ -41,34 +41,33 @@ export default function Send() {
       !newErrors.nickname &&
       !newErrors.journalEntry
     ) {
-      const formData = {
-        phoneNumber,
-        nickname,
-        journalEntry,
-      };
-      try {
-        const response = await fetch(
-          'https://2d56-163-152-3-142.ngrok-free.app/api/v1/post',
-          {
-            method: 'POST',
-            body: formData,
-          }
-        );
+      // const formData = {
+      //     phoneNumber,
+      //     nickname,
+      //     journalEntry,
+      // };
+      navigate('/submitted');
+      // try {
+      //     const response = await fetch('/api/submit', {
+      //         method: 'POST',
+      //         headers: {
+      //             'Content-Type': 'application/json',
+      //         },
+      //         body: JSON.stringify(formData),
+      //     });
 
-        const responseData = await response.json();
-
-        if (response.ok) {
-          console.log('Data submitted successfully:', responseData);
-          setPhoneNumber('');
-          setNickname('');
-          setJournalEntry('');
-          navigate('/Submitted');
-        } else {
-          console.error('Failed to submit data');
-        }
-      } catch (error) {
-        console.error('Error submitting data:', error);
-      }
+      //     if (response.ok) {
+      //         console.log('Data submitted successfully:', formData);
+      //         setPhoneNumber('');
+      //         setNickname('');
+      //         setJournalEntry('');
+      //         navigate('/Submitted');
+      //     } else {
+      //         console.error('Failed to submit data');
+      //     }
+      // } catch (error) {
+      //     console.error('Error submitting data:', error);
+      // }
     }
   };
 
@@ -77,89 +76,95 @@ export default function Send() {
       className="flex flex-col justify-start items-center w-full h-full bg-transparent px-4"
       style={{ height: '85%' }}
     >
-      <div className=" archive-grid w-full max-w-3xl space-y-4 p-4 overflow-auto">
-        <form className="flex flex-col w-full space-y-4 gap-2">
+      <div className="archive-grid w-full max-w-3xl space-y-4 p-4 overflow-auto">
+        <form className="flex flex-col items-center w-full space-y-4 gap-2">
           <div className="w-full h-full mb-5">
             <img
               className="h-auto w-1/6 m-auto"
               src="../../public/star.png"
               alt=""
             />
-            <p className="text-white text-center text-3xl mb-1 font-semibold">
+            <p className="text-white text-center text-3xl mb-1 mt-[12px] font-semibold">
               11월 9일의 조각글
             </p>
             <p className="text-purple-100 text-center text-base">
               나의 조각글을 남기고 다른 조각글을 받아봐요
             </p>
           </div>
-
           {/* 전화번호 필드 */}
-          <label className="text-purple-100 text-sm font-medium ">
-            전화번호
-            <span className=" text-xs ml-2 text-purple-200">
-              *전화번호로 비밀번호와 답변 알림을 보내드려요
-            </span>
-          </label>
-          <input
-            id="phone"
-            type="tel"
-            value={phoneNumber}
-            onChange={handlePhoneNumberChange}
-            placeholder="ex) 01011112222"
-            className={`text-white transition-all duration-100 form-input-tel px-5 py-2 rounded-3xl text-sm outline-none 
-                            focus:border-solid focus:border-white focus:border-2  ${
+          <div>
+            <div className="flex items-center justify-start">
+              <label className="text-purple-100 text-xs font-medium ">
+                전화번호
+              </label>
+              <span className="ml-2 text-white/60 text-[10px] font-normal font-['Pretendard Variable']">
+                *전화번호로 비밀번호와 답변 알림을 보내드려요
+              </span>
+            </div>
+            <input
+              id="phone"
+              type="tel"
+              value={phoneNumber}
+              onChange={handlePhoneNumberChange}
+              placeholder="ex) 01011112222"
+              className={`text-white transition-all duration-100 form-input-tel mt-[7px] px-5 py-2 text-xs outline-none 
+                            focus:border-solid focus:border-white focus:border w-[306px] h-9 bg-[#7763a5]/50 rounded-[18px] ${
                               errors.phoneNumber ? 'wrong' : ''
                             }`}
-            style={{ backgroundColor: '#6C548C' }}
-          />
-
+              style={{ backgroundColor: '#6C548C' }}
+            />
+          </div>
           {/* 닉네임 필드 */}
-          <label className="text-sm font-medium text-purple-100">닉네임</label>
-          <input
-            id="nickname"
-            type="text"
-            value={nickname}
-            onChange={handleNicknameChange}
-            placeholder="ex) nickname"
-            className={`text-white transition-all duration-100 form-input-tel px-5 py-2 rounded-3xl 
+          <div>
+            <div className="text-xs font-medium text-purple-100">닉네임</div>
+            <input
+              id="nickname"
+              type="text"
+              value={nickname}
+              onChange={handleNicknameChange}
+              placeholder="ex) nickname"
+              className={`text-white transition-all duration-100 form-input-tel w-[306px] mt-[7px] h-9 px-5 py-2 rounded-3xl 
                                     ${
                                       errors.nickname && nickname === ''
                                         ? 'wrong'
                                         : ''
-                                    } text-sm outline-none focus:border-solid focus:border-white focus:border-2`}
-            style={{ backgroundColor: '#6C548C' }}
-          />
-
-          {/* 조각글 필드 */}
-          <label className="text-sm font-medium text-purple-100">조각글</label>
-          <div className="relative w-full">
-            <textarea
-              id="journal"
-              value={journalEntry}
-              onChange={handleInputChange}
-              placeholder="어떤 이야기든 괜찮아요. 자유롭게 글을 작성해주세요."
-              rows="8"
-              maxLength={maxChars}
-              className={`w-full p-4 rounded-3xl focus:outline-none resize-none focus:border-solid focus:border-white focus:border-2
-                            text-white scroll-smooth text-sm ${
+                                    } text-xs outline-none focus:border-solid focus:border-white focus:border`}
+              style={{ backgroundColor: '#6C548C' }}
+            />
+          </div>
+          <div>
+            {/* 조각글 필드 */}
+            <label className="text-xs font-medium text-purple-100">
+              조각글
+            </label>
+            <div className="relative w-full">
+              <textarea
+                id="journal"
+                value={journalEntry}
+                onChange={handleInputChange}
+                placeholder="어떤 이야기든 괜찮아요. 자유롭게 글을 작성해주세요."
+                rows="8"
+                maxLength={maxChars}
+                className={`p-[13px] w-[306px] rounded-[18px] focus:outline-none resize-none  mt-[7px] focus:border-solid focus:border-white focus:border
+                            text-white scroll-smooth text-xs ${
                               errors.journalEntry && journalEntry === ''
                                 ? 'wrong'
                                 : ''
                             }`}
-              style={{ backgroundColor: '#6C548C' }}
-            />
-            <div className="absolute bottom-2 right-3 text-white text-sm">
-              {journalEntry.length}/{maxChars}
+                style={{ backgroundColor: '#6C548C' }}
+              />
+              <div className="absolute bottom-4 right-3 text-white/60 text-xs font-normal">
+                {journalEntry.length}/{maxChars}
+              </div>
             </div>
           </div>
+          <button
+            onClick={handleSubmit}
+            className="w-[306px] h-[39px] bg-white/80 py-2 text-[#2d1a58] text-xs rounded-full"
+          >
+            띄워보내기
+          </button>
         </form>
-
-        <button
-          onClick={handleSubmit}
-          className="w-full py-2 bg-white text-black rounded-full text-base "
-        >
-          띄워보내기
-        </button>
       </div>
     </div>
   );
